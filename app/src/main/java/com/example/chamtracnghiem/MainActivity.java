662b5882d4e9d7362da2b7f0075c24e31adc8b81
Duy Nguyen <khacduy17@gmail.com>
2020-04-24 15:07:29 +0200

    Hey I can take a photo!

--- app/src/main/java/com/example/chamtracnghiem/MainActivity.java
+++ app/src/main/java/com/example/chamtracnghiem/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.chamtracnghiem;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button takePhotoButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        takePhotoButton = (Button) findViewById(R.id.takePhoto);
+        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPhotoScreen();
+            }
+        });
+    }
+
+
+    public void openPhotoScreen(){
+        Intent intent = new Intent(this,OpenCamera.class);
+        startActivity(intent);
     }
 }
